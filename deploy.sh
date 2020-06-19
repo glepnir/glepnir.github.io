128b2249aa46e03aaee47744de9996beb4f260f1
glepnir <glepnir@gopherhub.org>
2020-06-19 20:39:35 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,41 @@
-#! /bin/bash
+#!/bin/bash
 
-set -eux
+if [[ $(git status -s) ]]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-msg="rebuilding site `date`"
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
 
-if [ $# -eq 1  ]
-    then msg="$1"
-fi
+echo "Checking out master branch into public"
+git worktree add -B master public origin/master
 
-# Build the project.
-hugo -d docs
+echo "Removing existing files"
+rm -rf public/*
 
-# Add changes to git.
-git add .
+echo "Generating site"
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+echo "Updating master branch"
+# Go To Public folder and add changes to git.
+cd public && git add --all
 
 # Commit changes.
-
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
 git commit -m "$msg"
 
-# Push source and build repos.
-git push origin master
+# Push source.
+git push -f origin master
+
+# Come Back up to the Project Root
+cd ..
